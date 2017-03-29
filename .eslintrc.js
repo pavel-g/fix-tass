331c00c99b420aeb6a24be51361160d4bac07f5e
Gnedov Pavel <pavel@gnedov.info>
2017-03-29 11:48:39 +0700

    Added filter for forums.overclockers.ru

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	"env": {
+		"browser": true
+	},
+	"extends": "eslint:recommended",
+	"rules": {
+		"no-console": "off",
+		// "no-undef": "off",
+		"comma-dangle": ["warn", "never"],
+		"no-unused-vars": "warn",
+		"no-debugger": "warn"
+	}
+};
--- fix-overclockers.js
+++ fix-overclockers.js
@@ -2,8 +2,31 @@ var Esprima = require('esprima');
 var Escodegen = require('escodegen');
 var JSPath = require('jspath');
 
-function FixOverclockers(content) {
-	// TODO: code for FixOverclockers
+function GetFixedCode() {
+	return Esprima.parse('console.log("Предотвращено исполнение плохого кода");').body[0];
 }
+
+function FixOverclockers(script) {
+	var parsed = Esprima.parse(script);
+	var filter = '..{.type === "BlockStatement" && .body{.expression.arguments{.value *== "Мы определили, что вы используете систему удаления рекламы"}}}';
+	var exprs = JSPath.apply(filter, parsed);
+	var changes = false;
+	if (exprs && exprs.length > 0) {
+		var i, expr;
+		for ( i = 0; i < exprs.length; i++ ) {
+			expr = exprs[i];
+			expr.body.length = 0;
+			expr.body.push(GetFixedCode());
+			changes = true;
+		}
+	}
+	if (changes) {
+		return Escodegen.generate(parsed)
+	} else {
+		return null;
+	}
+}
+
+FixOverclockers.GetFixedCode = GetFixedCode;
 
 module.exports = FixOverclockers;
